6c447aac3df3cd975a39a1ea3933e76eff2460a1
Student <student@itpark.ru>
2019-09-28 10:42:33 +0300

    photos and video

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 console.log('worked');
 
-const posts = {
-    
-}
+
+
+
+const author = {
+    id: 'buzova86',
+    avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com', 
+    verified: true,
+
+
+
+
+};
+
+const photo = {
+    id: 'B23d82NooNF',
+    author: author,
+    location: 'Набережная Амура. Граница с Китаем' ,
+    imageUrl: 'https://placekitten.com/800/800',
+    likes: 159594,
+    vidioUrl: null,
+};
+
+const video = {
+   id:'B24FlbJos5E',
+   author: author,
+   location: 'Торгово-развлекательный центр' ,
+   image: 'https://placekitten.com/800/800',
+   likes: 159594,
+   videoUrl: 'https://youtube.com/...',
+
+
+
+};
+console.log(photo.vidioUrl)
