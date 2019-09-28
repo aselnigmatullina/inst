45beb58767ba8be5090d2c2a163de127d002e391
Student <student@itpark.ru>
2019-09-28 10:59:36 +0300

    rename author

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('worked');
 
 
 
-const author = {
+const buzova = {
     id: 'buzova86',
     avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com', 
     verified: true,
@@ -15,7 +15,7 @@ const author = {
 
 const photo = {
     id: 'B23d82NooNF',
-    author: author,
+    author: buzova,
     location: 'Набережная Амура. Граница с Китаем' ,
     imageUrl: 'https://placekitten.com/800/800',
     likes: 159594,
@@ -24,7 +24,7 @@ const photo = {
 
 const video = {
    id:'B24FlbJos5E',
-   author: author,
+   author: buzova,
    location: 'Торгово-развлекательный центр' ,
    image: 'https://placekitten.com/800/800',
    likes: 159594,
